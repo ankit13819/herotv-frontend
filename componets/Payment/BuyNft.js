df9cf22a423a229cdd48f690683d8bd58688b0ae
ankit13819 <ankit1381997@gmail.com>
2025-04-14 10:40:34 +0530

    fixed

--- componets/Payment/BuyNft.js
+++ componets/Payment/BuyNft.js
@@ -1,5 +1,88 @@
-export default function BuyNowNft({ nftDetails }) {
-  console.log(nftDetails)
+'use client'
+import { useEffect, useState } from "react"
+import { Box, Divider, ListItem, Typography } from "@mui/material"
+import PaymentStyles from './styles'
+import Link from "next/link"
 
-  return <h1>BuyNowNft</h1>
+
+export default function BuyNowNft({ nftDetails }) {
+
+  const [nftTokenimage, setNftTokenimage] = useState(nftDetails?.attributes?.nft_images?.data?.[0]?.attributes?.url)
+  const [selectedNft, setSelectedNft] = useState([nftDetails?.attributes?.nft_images?.data])
+  console.log("selected", selectedNft)
+
+
+
+  return (
+    <>
+      <Box sx={PaymentStyles.mainContainer}>
+        <Typography sx={PaymentStyles.breadCrumb} component="p">
+          <Link href="/" style={{ color: "#fff" }}>Home</Link> / Payment
+        </Typography>
+
+        <Box sx={PaymentStyles.mainsection}>
+          <Box sx={PaymentStyles.leftPanel}>
+            <Typography variant="h1" sx={PaymentStyles.heading}>
+              {nftDetails?.attributes?.description_asset_tier_mnemonics}
+            </Typography>
+
+            <Box sx={PaymentStyles.imageSection}>
+              <Box component="img" src={nftTokenimage}>
+              </Box>
+
+              <Box sx={PaymentStyles.thumbnailContainer} className="hidden-scroll-bar">
+                {
+                  selectedNft?.length > 0 &&
+                  selectedNft?.map((item) => item.map((data) => {
+                    return (
+                      <ListItem
+                        key={data?.id}
+                        className={data?.attributes?.url == nftTokenimage && "active"}
+                      >
+                        <Box
+                          style={{ cursor: "pointer" }}
+                          className="active"
+                          key={data.id}
+                          component="img"
+                          src={data?.attributes?.url}
+                          onClick={() => {
+                            setNftTokenimage(data?.attributes?.url);
+                          }}
+                        />
+                      </ListItem>
+                    );
+                  }))
+                }
+              </Box>
+            </Box>
+
+            <Box sx={PaymentStyles.PriceSection}>
+              {/* but apply condition IN and Other we do after sometime */}
+              <Typography variant="h3">Price - $67</Typography>
+            </Box>
+          </Box>
+
+          <Box sx={PaymentStyles.rightPanel}>
+            <Divider sx={PaymentStyles.separatorLine1}>
+              <Typography variant="span" sx={PaymentStyles.walletBalance}>
+                <Box component="img" src="/images/payment/wallet.svg" width="24" height="24" />
+                &nbsp;Wallet Balance &nbsp;
+                <span style={{ fontWeight: '400' }}>$55</span>
+                &nbsp; &nbsp; &nbsp;
+                <Box
+                  component="img"
+                  src="/images/newHome/headercoin.svg"
+                  width="24px"
+                  height="24px"
+                />
+                &nbsp;
+                coin
+              </Typography>
+            </Divider>
+          </Box>
+
+        </Box>
+      </Box>
+    </>
+  )
 }
--- src/app/payment/buyNowNft/page.js
+++ src/app/payment/buyNowNft/page.js
@@ -1,4 +1,6 @@
+'use client'
 import { Box } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import BuyNowNft from "../../../../componets/Payment/BuyNft";
 
 const nftDetails =
@@ -6,21 +8,102 @@ const nftDetails =
   data: [
     {
       id: 1,
-      title: "ankit"
+      title: "ankit",
+      attributes: {
+        description_asset_tier_mnemonics: "GOLD",
+        nft_images: {
+          data: [
+            {
+              id: 1,
+              attributes: {
+                url: '/images/nft/1.png'
+              }
+            },
+            {
+              id: 2,
+              attributes: {
+                url: '/images/nft/2.png'
+              }
+            },
+            {
+              id: 3,
+              attributes: {
+                url: '/images/nft/3.png'
+              }
+            }
+          ]
+        }
+      },
     },
     {
       id: 2,
-      title: "Ankita"
+      title: "Silver",
+      attributes: {
+        description_asset_tier_mnemonics: "SILVER",
+        nft_images: {
+          data: [
+            {
+              id: 2,
+              attributes: {
+                url: '/images/nft/2.png'
+              }
+            }
+          ]
+        }
+      },
+
     }
   ]
 }
 
 
-export default function ReserveBuyNowNft() {
+const useStyles = makeStyles((theme) => ({
+  mainContainer: {
+    backgroundColor: "#0c091b",
+    color: "#ffffff",
+    maxWidth: "100%",
+    margin: "auto",
+    fontFamily: "'Inter',sans-serif !important",
+  },
+}))
 
+const styles = {
+  faqContainer: {
+    position: "fixed",
+    right: "-30px",
+    top: "50%",
+    zIndex: "1",
+    transform: "rotate(90deg)",
+
+    "& button": {
+      cursor: "pointer",
+      color: "custom.faqBtn",
+      padding: "8px 35px",
+      borderRadius: "0px 0px 10px 10px",
+      boxShadow: "0 0 6px 0 rgba(157, 96, 212, 0.5)",
+      border: "solid 2px transparent",
+      background: "linear-gradient(93.2deg, #E14184 0%, #3555FA 55.26%, #55B5BB 97.94%)",
+      backgroundOrigin: "border-box",
+      boxShadow: (theme) => ({ xs: theme.palette.boxShadow.faqButton }),
+      fontSize: "14px",
+      textTransform: "uppercase",
+      "@media(max-width:767px)": {
+        padding: "3px 10px",
+        fontSize: "13px",
+      },
+    },
+    "@media(max-width:767px)": {
+      right: "-10px",
+    },
+  },
+};
+
+
+export default function ReserveBuyNowNft() {
+  const classes = useStyles()
 
   return <>
-    <Box>
+    <Box className={classes.mainContainer}>
       <BuyNowNft nftDetails={nftDetails?.data?.[0]} />
     </Box>
   </>
